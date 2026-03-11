26da47342de523556db79470833bfac7e206aef5
apkor028 <francisbernardo023@gmail.com>
2026-03-12 00:24:20 +0800

    amend: update projects.js

--- projects.js
+++ projects.js
@@ -2,4 +2,8 @@ const projects = document.querySelector(".projects");
 
 projects.addEventListener("click", function () {
     window.location.href = "projects.html";
+}); 
+const about = document.querySelector(".about");
+about.addEventListener("click", function () {
+    window.location.href = "about.html";
 });
